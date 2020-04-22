d942409014115b596b0fe26cdb36d2734b7cda58
rucsa <ruxandra.vulpoiu@gmail.com>
2020-04-22 13:43:01 +0300

    Updates

---
+++ precache-manifest.6cb2ff22e74b4086d35b04b0d60d1233.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1051592b02f23dd9d68cf75b17a3b2f",
+    "url": "/tc_under_construction/index.html"
+  },
+  {
+    "revision": "614e0ca32d12cf2c6c1e",
+    "url": "/tc_under_construction/static/css/main.0bdeb0d7.chunk.css"
+  },
+  {
+    "revision": "f68e9c90bf08e43ec121",
+    "url": "/tc_under_construction/static/js/2.88a5e011.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tc_under_construction/static/js/2.88a5e011.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "614e0ca32d12cf2c6c1e",
+    "url": "/tc_under_construction/static/js/main.5fee1775.chunk.js"
+  },
+  {
+    "revision": "e2fe3e04e1db762bc1d8",
+    "url": "/tc_under_construction/static/js/runtime-main.79aaf603.js"
+  },
+  {
+    "revision": "abda0be2ea47e403f6f56cc8408f9105",
+    "url": "/tc_under_construction/static/media/catcher.abda0be2.svg"
+  },
+  {
+    "revision": "2a1cacfcf95c3ce4434a992acd7f7ba0",
+    "url": "/tc_under_construction/static/media/flower.2a1cacfc.svg"
+  },
+  {
+    "revision": "5bd211ba17f889996fd25bf0a4f57c7c",
+    "url": "/tc_under_construction/static/media/vinyl.5bd211ba.svg"
+  }
+]);
