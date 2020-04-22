2f5ec0ef96e7730c2caea64b7fd5613b25c5b867
rucsa <ruxandra.vulpoiu@gmail.com>
2020-04-22 16:04:38 +0300

    v2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import catcher from './catcher.svg';
 import flower from './flower.svg';
 import vinyl from './vinyl.svg';
 import './App.css';
@@ -10,10 +9,9 @@ function App() {
       <header className="App-header">
         <img src={vinyl} className="App-vinyl" alt="logo" />
         <img src={flower} className="App-flower" alt="logo" />
-        <img src={catcher} className="App-catcher" alt="logo" />
         <div className="text">
           <h1>Something trippy is under construction here</h1>
-          <p> Until then #stayhome.</p>
+          <p> Until then, #stayhome.</p>
         </div>
       </header>
     </div>
